71fd12237064fe3b1c55b62051a70a896e03e98e
Praveen Sharma <praveensharma.inbox@gmail.com>
2019-12-15 22:31:53 +0530

    Few Corrections

--- src/models/Conversation.js
+++ src/models/Conversation.js
@@ -1,13 +1,16 @@
 import mongoose from 'mongoose'
 import MessageType from "./MessageType";
-
+import User from "./User"
 
 const conversationSchema = new mongoose.Schema({
     between_users :[mongoose.Schema.Types.ObjectId],
     type : MessageType._id,
     messages:[{
         text:String,
-        user_id:mongoose.Schema.Types.ObjectId,
+        user_id:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'User'
+        },
         timestamp:Number
     }]
 })
