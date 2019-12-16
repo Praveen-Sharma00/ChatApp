5da8b219a913ed8ba6295300547e5dd8a3f7e3a4
Praveen <praveensharma.inbox@gmail.com>
2019-12-16 15:25:22 +0530

    dashboard : Add Contact form

--- public/assets/js/custom/chat.js
+++ public/assets/js/custom/chat.js
@@ -43,6 +43,34 @@ let socket = io();
 })()
 
 
+async function addContact(){
+    const name = document.getElementById('nick_name').value
+    const email=document.getElementById('email').value
+
+    if(name===""||email==="")
+    {
+        return alert("Fill all fields")
+    }
+    const response = await fetch('http://localhost:3000/contact',{
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body:JSON.stringify({
+            name:name,
+            email:email
+        })
+    })
+    const data = await response.json()
+    if(data.error){
+    //  return alert(data.error)
+     return alert(data.msg)
+    }else if(data.success){
+        return alert('Contact added')
+    }
+}
+
+
 
 
 // socket.emit('join', 'A')
