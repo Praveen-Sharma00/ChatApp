3935036fb8cc42c23626030f6f1eb619a202817f
Praveen <praveensharma.inbox@gmail.com>
2020-01-21 11:22:36 +0530

    Socket Class created

---
+++ New/frontend/src/utils/socket.js
@@ -0,0 +1,16 @@
+import io from "socket.io-client";
+
+export default class SocketIO{
+    constructor(store){
+        this.store=store
+        this.socket=io('http://localhost:3000/chat')
+    }
+    listen(){
+        this.socket.on('new-message',(data)=>{
+            console.log(data)
+        })
+    }
+    emit(data){
+        this.socket.emit('new-message',data)
+    }
+}
