8f9a5b5ccd01a9a95c2235dcc15dfbc0f92db242
Praveen <praveensharma.inbox@gmail.com>
2020-01-29 12:42:22 +0530

    Fixed: Default routes

--- Current/frontend/src/router/auth.js
+++ Current/frontend/src/router/auth.js
@@ -2,6 +2,7 @@ import Login from "../components/Auth/Login";
 import Register from "../components/Auth/Register";
 
 export default [
-    {path:'/login',component:Login},
-    {path:'/register',component: Register}
+    {path:'/login',component:Login,name:'Login'},
+    {path:'/register',component: Register,name:'Register'},
+    {path:'',redirect:{name:'Login'}}
 ]
